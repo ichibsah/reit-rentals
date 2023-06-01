0eb76a9a2fa113b5c0e6ad00ffe6f51a5d582577
ibrahim <ibrahim@virthost01.skyline.lan>
2023-06-01 21:52:34 +0200

    Thu Jun  1 21:52:34 CEST 2023

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM python:3.9-slim-buster as stage-app
 
 COPY . .
-COPY requirements.txt requirements.txt
+#COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
+
+RUN source ./venv/bin/activate
 
 #WORKDIR /code
 #RUN python manage.py runserver
@@ -10,7 +12,7 @@ RUN pip install -r requirements.txt
 #CMD ["gunicorn", "--config", "gunicorn.conf.py", "dentist:website"]
 
 # Expose ports
-EXPOSE 80
+EXPOSE 8000
 
 # Expose volumes
 #VOLUME ["/etc/nginx/conf.d", "/var/log/nginx", "/www"]
--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 docker rm rentals -f
 docker build -t rentals:latest -t rentals:v1.0 . 
 #docker build -t rentals . 
-docker run -it -d -p 8000:80 --name rentals rentals:latest
+docker run -it -d -p 8000:8000 --name rentals rentals:latest
 #docker run -it -d -p 8000:80 -v /home/ibrahim/sandbox/meryem/logs:/var/log/nginx:rw --name rentals rentals:latest
 #docker run -it -d -p 8090:80 --restart always -v /home/ibrahim/sandbox/anasse/website:/www -v /home/ibrahim/sandbox/anasse/logs:/var/log/nginx:rw --name anasse anasse 
 #docker run -it meryem 
