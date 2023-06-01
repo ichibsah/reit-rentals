afc8aa5cf4f56599f225349615f21696375a8d92
ibrahim <ibrahim@virthost01.skyline.lan>
2023-06-01 21:31:09 +0200

    Thu Jun  1 21:31:09 CEST 2023

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 docker build -t rentals:latest -t rentals:v1.0 . 
 #docker build -t rentals . 
-#docker run -it -d -p 8090:80 -v /home/ibrahim/sandbox/meryem/logs:/var/log/nginx:rw --name meryem meryem 
+docker run -it -d -p 8000:80 --name rentals rentals:latest
+#docker run -it -d -p 8000:80 -v /home/ibrahim/sandbox/meryem/logs:/var/log/nginx:rw --name rentals rentals:latest
 #docker run -it -d -p 8090:80 --restart always -v /home/ibrahim/sandbox/anasse/website:/www -v /home/ibrahim/sandbox/anasse/logs:/var/log/nginx:rw --name anasse anasse 
 #docker run -it meryem 
 #docker run -it node
